f81909ef4726bc82af65fd14bd775d3b575dbfa2
andrewtheyan <srdrewyan@gmail.com>
2020-02-13 21:38:50 -0800

    added random algo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,39 @@
 class ApplicationController < ActionController::Base
+
+    #list of the project ids
+    proj = [1,2,3,4,5]
+    
+    p_preference = {student_id:[1,2,3], student_id1: [3,4,2]}
+    #students with student ids
+    student = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].shuffle
+
+
+    def randomAlgo (proj, student)
+        result = {}
+        proj.each do |x|
+            result[x]=[]
+            # puts result[x].inspect
+        end
+        # puts result.inspect
+        group_size = (student.size/ proj.size)
+        temp_project_index = 0
+    
+        student.each do |x|
+    
+            
+    
+            while(result[proj[temp_project_index]].size>group_size)
+                temp_project_index = (temp_project_index+1) % proj.size
+            end
+            #puts "before" + result.inspect
+            result[proj[temp_project_index]].append(x)
+            temp_project_index = (temp_project_index+1) % proj.size
+            
+    
+        end
+    
+    
+    
+    end
+
 end
