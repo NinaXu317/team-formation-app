d0b52548684f084e68b7a18b9e43e184192443bd
reedandchrissy <lucicazhai@gmail.com>
2020-03-12 15:21:43 -0400

    edit preference

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -6,4 +6,25 @@ class PreferencesController < ApplicationController
     def new 
         @preference = Preference.new
     end
+
+    def create
+        @preference = Preference.new(preference_params)   
+        if @preference.save
+          flash[:success] = "Thanks for submitting your preference!"
+          redirect_to @preference.student
+        else
+          render 'new'
+        end
+    end
+    
+    private
+    
+    def preference_params
+          params.require(:preference).permit(:first, :second, :third)
+    end
 end
+    
+
+
+    
+
