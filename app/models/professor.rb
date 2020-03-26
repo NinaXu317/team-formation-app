dbf237c3a32e265c2e86439ec9f31de390df427f
reedandchrissy <lucicazhai@gmail.com>
2020-03-26 07:14:24 -0400

    frontview edit

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,19 @@
 class Professor < ApplicationRecord
     has_many :courses
     has_secure_password
+
+    before_save { self.email = email.downcase }
+    validates :firstname,  presence: true, length: { maximum: 50 }
+    validates :lastname,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, 
+            format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+    def Professor.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,20 @@ class Student < ApplicationRecord
     has_many :courses, through: :takings
     has_many :groups, through: :takings
     accepts_nested_attributes_for :courses, allow_destroy: true
+    
+
+    before_save { self.email = email.downcase }
+    validates :firstname,  presence: true, length: { maximum: 50 }
+    validates :lastname,  presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, 
+            format: { with: VALID_EMAIL_REGEX }, 
+            uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+    def Student.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
