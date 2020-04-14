6c0f1fb01097a6b5e3651eb9143f7e31e82a3553
reedandchrissy <lucicazhai@gmail.com>
2020-04-14 11:38:42 -0400

    edit profile page

--- app/helpers/takings_helper.rb
+++ app/helpers/takings_helper.rb
@@ -1,16 +1,14 @@
 module TakingsHelper
-    def projectName(student, course)
-        pj_name = nil
+    def group(student, course)
         takings = student.takings
+        group = nil
         if !takings.nil?
             courses = student.takings.where(course_id: course['id'])
             if !courses.nil?
                 group = student.takings.where(course_id: course['id'])[0].group
-                if !group.nil?
-                    pj_name = group['project_name']
-                end
             end
         end
-        return pj_name
+        return group
     end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 require("jquery")
 require("moment")
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
