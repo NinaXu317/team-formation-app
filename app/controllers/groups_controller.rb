832c2061fec35a276a4e8417a4d9eca75ea9baa3
reedandchrissy <lucicazhai@gmail.com>
2020-04-24 17:32:41 -0400

    add description

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -81,6 +81,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:course_id, :project_name)
+      params.require(:group).permit(:course_id, :project_name, :description)
     end
 end
