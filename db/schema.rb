cea742113941e35bf9f16853f97480b8f2a2dd7c
reedandchrissy <lucicazhai@gmail.com>
2020-04-24 17:57:13 -0400

    add vote to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_212818) do
+ActiveRecord::Schema.define(version: 2020_04_24_215614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_212818) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
     t.text "description"
+    t.integer "vote"
   end
 
   create_table "holdprojects", force: :cascade do |t|
