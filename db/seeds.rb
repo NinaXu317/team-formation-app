d26f82b19b0d62882956144013b711f70f037ade
ditsky <samruditsky@yahoo.com>
2020-04-27 23:16:26 -0400

    editing seed file for demo day

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,9 @@ Group.reset_pk_sequence
 Course.reset_pk_sequence
 Taking.reset_pk_sequence
 
+#Add 100 students into the database
+#Email : lastname@brandeis.edu
+#Password for everyone: password
 (1..100).each do
     first = Faker::Name.first_name
     last = Faker::Name.last_name
@@ -21,11 +24,14 @@ Taking.reset_pk_sequence
                         password: "password")
 end
 
+#Create an admin student for development use
 admin_student = Student.create(firstname: "admin",
     lastname: "admin",
     email: "admin@admin.com",
     password: "password")
 
+#Add 50 professors into the database
+#Password for everyone: password
 (1..50).each do
     p = Professor.create(firstname: Faker::Name.first_name,
     lastname: Faker::Name.last_name,
@@ -33,51 +39,58 @@ admin_student = Student.create(firstname: "admin",
     password: "password")
 end
 
+#Create an admin professor for development use
 admin_professor = Professor.create(firstname: "admin",
     lastname: "admin",
     email: "admin@admin.com",
     password: "password")
 
+#Create 10 courses taught by the admin for demo use
 10.times do
     c = Course.create(name: Faker::Educator.course_name,
     pin: Faker::Number.number(digits: 4),
     professor_id: admin_professor.id)
 end
 
+#Create a consistent course taught by the admin professor for demo use
+consistent_course = Course.create(name: Faker::Educator.course_name,
+    pin: 1111, professor_id: admin_professor.id)
+
+#Add 30 courses into the database
 30.times do
     c = Course.create(name: Faker::Educator.course_name,
     pin: Faker::Number.number(digits: 4),
     professor_id: Professor.all.sample.id)
 end
 
-5.times do
-    g = Group.create(course_id: admin_professor.courses.all.sample.id,
-    project_name: Faker::Team.name, description: Faker::Game.genre)
-end
-
+#Add 60 groups into the database
 60.times do
     g = Group.create(course_id: Course.all.sample.id,
     project_name: Faker::Team.name, description: Faker::Game.genre)
 end
 
-100.times do 
-    g = Group.create(course_id: Course.where(professor_id: admin_professor.id).sample,
-                    project_name: Faker::Team.name)
+#Add 30 groups into the admin professor's classes for demo uses
+30.times do 
+    g = Group.create(course_id: Course.where(professor_id: admin_professor.id).sample.id,
+                    project_name: Faker::Team.name, description: Faker::Game.genre)
 end
 
-30.times do
+#Add 30 students into the admin professor's courses for demo uses
+100.times do
     n = admin_professor.courses.all.sample.id
     s = Student.all.sample.id
-    #t = Taking.create(student_id: s, course_id: n, group_id: Group.where(course_id: n).sample.id)
     t = Taking.create(student_id: s, course_id: n)
 end
 
-200.times do
+#Enroll random students in a random class 200 times
+20.times do
   n = Course.all.sample.id
   s = Student.all.sample.id
   t = Taking.create(student_id: s, course_id: n)
 end
 
+#Add a preference for each student in all of the admin professor's
+#courses for demo purposes
 admin_professor.courses.each do |course|
     groups = course.groups
     if groups.size > 0
